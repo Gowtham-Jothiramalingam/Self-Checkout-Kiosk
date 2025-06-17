d1b8ab1e87ae6c57f1cf8bafe2dc93db08cf9aa4
Gowtham <88978213+Gowtham-Jothiramalingam@users.noreply.github.com>
2025-06-17 16:31:07 +0530

    Update caliberation.ino

--- src/Esp/caliberation/caliberation.ino
+++ src/Esp/caliberation/caliberation.ino
@@ -1,15 +1,4 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/arduino-load-cell-hx711/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
 
-// Calibrating the load cell
 #include "HX711.h"
 
 // HX711 circuit wiring
